784c5cd358a93d63ea907fbfa26bfb76907aca9a
Fabio Petrone <fa610movic90@gmail.com>
2021-02-16 21:37:43 +0100

    bonus con prove partials/mixins

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,5 @@
 let mix = require('laravel-mix');
 
 mix.sass('src/app.scss', 'dist/').options({ processCssUrls: false });
+
+mix.sass('src/app-bonus.scss', 'dist/').options({ processCssUrls: false });
